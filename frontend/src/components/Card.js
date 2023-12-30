30fe668cf07456d757c49e81c02878919b230c1f
Burunova Olga <burun-olya@yandex.ru>
2023-12-30 12:11:36 +0300

    likes

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -8,9 +8,9 @@ function Card({ card, onCardClick, onCardDelete, onCardLike })
   const handleLike = () => onCardLike(card);
   const handleDelete = () => onCardDelete(card);
   const isOwn = card.owner === currentUser._id;
-  console.log(card.owner);  
-  console.log(currentUser._id);  
   const isLiked = card.likes.some(i => i._id === currentUser._id);
+  console.log(card.likes);
+  console.log(isLiked);
 
   return (
     <div className="element">
